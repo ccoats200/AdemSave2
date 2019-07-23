cdf6690ea61847e79cb931f1da13eb199616fa7e
ccoats200 <ccoats200@gmail.com>
2019-07-23 06:59:27 -0400

    controllers

---
+++ AdemSave/Controllers/ProductInfoVC.swift
@@ -0,0 +1,24 @@
+//
+//  ProductInfoVC.swift
+//  AdemSave
+//
+//  Created by Coleman Coats on 7/23/19.
+//  Copyright © 2019 Coleman Coats. All rights reserved.
+//
+
+import UIKit
+
+class productInfoVC: UIViewController {
+    
+    //Reuse ID
+    let itemViewControllerDesign = "product"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
---
+++ AdemSave/Controllers/SearchVC.swift
@@ -0,0 +1,52 @@
+//
+//  SearchVC.swift
+//  AdemSave
+//
+//  Created by Coleman Coats on 7/23/19.
+//  Copyright © 2019 Coleman Coats. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class searchControllers: CellBasics {
+    
+    let searchBars: UISearchBar = {
+        let bar = UISearchBar()
+        bar.translatesAutoresizingMaskIntoConstraints = false
+        bar.barTintColor = UIColor.clear
+        
+        
+        return bar
+    }()
+    
+    override func setupViews() {
+        
+        addSubview(searchBars)
+        print("adds search bar to header view")
+        
+        //searchBar.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        //searchBar.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        //searchBar.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        addConstraintsWithFormats(format: "H:|[v0]|", views: searchBars)
+        addConstraintsWithFormats(format: "V:|[v0]|", views: searchBars)
+        
+        
+    }
+}
+
+
+class headerViews: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = UIColor.red
+        self.layer.cornerRadius = 5
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
