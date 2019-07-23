e801c0e6bedc9e04c91195b00bde2906b797842b
ccoats200 <ccoats200@gmail.com>
2019-07-23 07:03:47 -0400

    a few more

---
+++ AdemSave/Extensions/AppDelegateUserLogin.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegateUserLogin.swift
+//  AdemSave
+//
+//  Created by Coleman Coats on 7/23/19.
+//  Copyright © 2019 Coleman Coats. All rights reserved.
+//
+
+
+
+import Foundation
+import UIKit
+
+//extension AppDelegate {
+//    static var shared: AppDelegate {
+//        return UIApplication.shared.delegate as! AppDelegate
+//}
